d53408c9357d866f8dabf032aaf57a3f4b1cb88d
gladimir777 <gladimir777@github.com>
2020-07-15 22:23:55 -0400

    Fixing onclick issues

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Alert = ({ msg, error }) => {
+  const [close, setClose] = useState(true);
+  const handleClick = () => {
+    this.parentElement.style.display = "none";
+  };
   return (
     <div className={`alert ${error ? "error-msg" : ""}`}>
-      <span
-        className="closebtn"
-        onClick={(this.parentElement.style.display = "none")}
-      >
+      <span className="closebtn" onClick={handleClick}>
         &times;
       </span>
       <strong>Error!</strong>
